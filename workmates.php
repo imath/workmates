790b208f8c808fa5555fd4a6d1a6caf4f415eb1d
imath <math.viet@gmail.com>
2018-05-18 10:07:21 +0200

    Improve plugin header tags and loader

--- workmates.php
+++ workmates.php
@@ -1,19 +1,13 @@
 <?php
 /**
- * @package   WorkMates
- * @author    imath https://twitter.com/imath
- * @license   GPL-2.0+
- * @link      http://imathi.eu
- * @copyright 2013 imath
- *
- * @wordpress-plugin
+ * @buddypress-plugin
  * Plugin Name:       WorkMates
  * Plugin URI:        http://imathi.eu/tag/workmates/
- * Description:       Customizes BuddyPress on some behaviors (that i've needed) for its use in a company
- * Version:           1.0-beta3
+ * Description:       Intègre l'interphace des invitations de groupe du pack de gabarits de Nouveau dans celui de l'héritage de BP Default.
+ * Version:           2.0.0-alpha
  * Author:            imath
  * Author URI:        http://imathi.eu
- * Text Domain:       rendez-vous
+ * Text Domain:       workmates
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Domain Path:       /languages/
@@ -53,7 +47,7 @@ class WorkMates {
 		'component_id'        => 'workmates',
 		'component_root_slug' => 'workmates',
 		'component_name'      => 'WorkMates',
-		'bp_version_required' => '2.2'
+		'bp_version_required' => '3.0'
 	);
 
 	/**
@@ -94,7 +88,7 @@ class WorkMates {
 	 */
 	private function setup_globals() {
 		/** WorkMates globals ********************************************/
-		$this->version                = '1.0-beta3';
+		$this->version                = '2.0.0-alpha';
 		$this->domain                 = 'workmates';
 		$this->file                   = __FILE__;
 		$this->basename               = plugin_basename( $this->file );
@@ -108,9 +102,9 @@ class WorkMates {
 
 		/** Component specific globals ********************************************/
 		// Maybe one day... but not used right now
-		$this->component_id         = self::$init_vars['component_id'];
-		$this->component_slug       = self::$init_vars['component_root_slug'];
-		$this->component_name       = self::$init_vars['component_name'];
+		$this->component_id   = self::$init_vars['component_id'];
+		$this->component_slug = self::$init_vars['component_root_slug'];
+		$this->component_name = self::$init_vars['component_name'];
 
 		//Group specific
 		$this->group_component_name = 'Invite Workmates';
@@ -126,8 +120,9 @@ class WorkMates {
 	 */
 	public static function buddypress_version_check() {
 		// taking no risk
-		if( !defined( 'BP_VERSION' ) )
+		if ( ! defined( 'BP_VERSION' ) ) {
 			return false;
+		}
 
 		return version_compare( BP_VERSION, self::$init_vars['bp_version_required'], '>=' );
 	}
@@ -139,13 +134,13 @@ class WorkMates {
 	 * @since 1.0
 	 */
 	public static function buddypress_site_check() {
-		global $blog_id;
-
-		if( !function_exists( 'bp_get_root_blog_id' ) )
+		if ( ! function_exists( 'bp_get_root_blog_id' ) ) {
 			return false;
+		}
 
-		if( $blog_id != bp_get_root_blog_id() )
+		if ( (int) get_current_blog_id() !== (int) bp_get_root_blog_id() ) {
 			return false;
+		}
 
 		return true;
 	}
@@ -157,17 +152,17 @@ class WorkMates {
 	 * @since 1.0
 	 */
 	private function includes() {
-		require( $this->includes_dir . 'functions.php' );
-		require( $this->includes_dir . 'filters.php' );
-		require( $this->includes_dir . 'ajax.php' );
-		require( $this->includes_dir . 'screens.php' );
-		require( $this->includes_dir . 'editor.php' );
-		require( $this->includes_dir . 'classes.php' );
-		require( $this->includes_dir . 'template.php' );
+		require $this->includes_dir . 'functions.php';
+		require $this->includes_dir . 'filters.php';
+		require $this->includes_dir . 'ajax.php';
+		require $this->includes_dir . 'screens.php';
+		require $this->includes_dir . 'editor.php';
+		require $this->includes_dir . 'classes.php';
+		require $this->includes_dir . 'template.php';
 
-		if( bp_is_active( 'groups' ) && ! bp_is_active( 'friends' ) && self::buddypress_version_check() )
-			require( $this->includes_dir . 'groups.php' );
-
+		if ( bp_is_active( 'groups' ) && ! bp_is_active( 'friends' ) && self::buddypress_version_check() ) {
+			require $this->includes_dir . 'groups.php';
+		}
 	}
 
 	/**
@@ -178,8 +173,9 @@ class WorkMates {
 	 */
 	private function setup_hooks() {
 		// Bail if BuddyPress version is not supported or current blog is not the one where BuddyPress is activated
-		if( ! self::buddypress_version_check() || ! self::buddypress_site_check() )
+		if ( ! self::buddypress_version_check() || ! self::buddypress_site_check() ) {
 			return;
+		}
 
 		//Actions
 		add_action( 'bp_init',                   array( $this, 'load_textdomain'  ), 6 );
@@ -187,15 +183,14 @@ class WorkMates {
 		add_action( 'bp_messages_setup_globals', array( $this, 'autocomplete_all' )    );
 
 		//Filters
-		if( bp_is_active( 'groups' ) ) {
+		if ( bp_is_active( 'groups' ) ) {
 			add_filter( 'groups_forbidden_names', array( $this, 'groups_forbidden_names' ), 10, 1 );
 		}
 
-		if( bp_is_active( 'messages' ) ) {
+		if ( bp_is_active( 'messages' ) ) {
 			add_filter( 'bp_core_search_users_count_sql', 'workmates_filter_message_ac_count', 10, 1 );
 			add_filter( 'bp_core_search_users_sql', 'workmates_filter_message_ac_select', 10, 3 );
 		}
-
 	}
 
 	/**
@@ -210,10 +205,9 @@ class WorkMates {
 	 * @uses bp_is_active() to check a component is active
 	 */
 	public function autocomplete_all( ) {
-		$bp = buddypress();
-
-		if( ! bp_is_active( 'friends' ) && bp_is_active( 'messages' ) )
-			$bp->messages->autocomplete_all = true;
+		if ( ! bp_is_active( 'friends' ) && bp_is_active( 'messages' ) ) {
+			buddypress()->messages->autocomplete_all = true;
+		}
 	}
 
 	/**
@@ -235,11 +229,11 @@ class WorkMates {
 	 * @uses workmates_enqueue_editor() to load a specific version of WP Media Editor
 	 */
 	public function cssjs() {
-
-		if( ! bp_is_active( 'groups' ) )
+		if ( ! bp_is_active( 'groups' ) ) {
 			return;
+		}
 
-		if( workmates_is_group_front() || workmates_is_group_create() ) {
+		if ( workmates_is_group_front() || workmates_is_group_create() ) {
 			$suffix = SCRIPT_DEBUG ? '' : '.min';
 
 			wp_register_script( 'workmates-plupload', includes_url( "js/plupload/wp-plupload$suffix.js" ), array(), $this->version, 1 );
@@ -253,7 +247,6 @@ class WorkMates {
 			// Enqueues a specific WP Media Editor (with no support for file upload)
 			workmates_enqueue_editor();
 		}
-
 	}
 
 	/**
@@ -298,11 +291,11 @@ class WorkMates {
 
 }
 
+endif;
+
 // Let's start !
 function workmates() {
 	return WorkMates::get_instance();
 }
 // Not too early and not too late ! 9 seems ok ;)
 add_action( 'bp_include', 'workmates', 9 );
-
-endif;
